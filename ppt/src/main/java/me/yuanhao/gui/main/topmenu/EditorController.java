e9f9e7de7ec637061df06556e33090eb0dbee286
Eminem-x <1773997586@qq.com>
2021-11-03 15:20:14 +0800

    Add EditorController.java

--- ppt/src/main/java/me/yuanhao/gui/main/topmenu/EditorController.java
+++ ppt/src/main/java/me/yuanhao/gui/main/topmenu/EditorController.java
@@ -1,4 +1,22 @@
 package me.yuanhao.gui.main.topmenu;
 
+import com.jfoenix.controls.JFXListView;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+/**
+ * @author Yuanhao
+ */
 public class EditorController {
+    @FXML
+    private JFXListView<?> toolbarPopupList;
+
+    @FXML
+    private void edit() {
+        if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 1) {
+            Platform.exit();
+        } else if(toolbarPopupList.getSelectionModel().getSelectedIndex() == 0) {
+            System.out.println(123456);
+        }
+    }
 }
