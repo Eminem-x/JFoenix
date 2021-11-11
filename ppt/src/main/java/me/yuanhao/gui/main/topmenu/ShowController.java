5674098151c1ffbcb1036a02b7606fb9e8e19808
Eminem-x <1773997586@qq.com>
2021-11-11 15:32:59 +0800

    Update ShowController.java

--- ppt/src/main/java/me/yuanhao/gui/main/topmenu/ShowController.java
+++ ppt/src/main/java/me/yuanhao/gui/main/topmenu/ShowController.java
@@ -2,7 +2,6 @@ package me.yuanhao.gui.main.topmenu;
 
 import com.jfoenix.controls.JFXListView;
 import javafx.fxml.FXML;
-import me.yuanhao.mapping.Loader;
 
 import java.lang.reflect.Method;
 
@@ -16,10 +15,7 @@ public class ShowController {
     @FXML
     private void show() throws Exception{
         if (toolbarPopupList.getSelectionModel().getSelectedIndex() == Function.ShowFromStart.ordinal()) {
-            Class<?> aClass = Class.forName("me.yuanhao.mapping.Loader");
-            Object o = aClass.newInstance();
-            Method method = aClass.getMethod("test");
-            method.invoke(o);
+
         } else if (toolbarPopupList.getSelectionModel().getSelectedIndex() == Function.ShowFromCurrent.ordinal()) {
 
         }
