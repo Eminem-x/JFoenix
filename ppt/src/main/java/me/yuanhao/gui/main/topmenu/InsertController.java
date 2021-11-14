74419875193eaba2928f2481da20997bfdce95db
Eminem-x <1773997586@qq.com>
2021-11-14 15:13:16 +0800

    Update InsertController.java

--- ppt/src/main/java/me/yuanhao/gui/main/topmenu/InsertController.java
+++ ppt/src/main/java/me/yuanhao/gui/main/topmenu/InsertController.java
@@ -1,8 +1,14 @@
 package me.yuanhao.gui.main.topmenu;
 
 import com.jfoenix.controls.JFXListView;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import me.yuanhao.mapping.Loader;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * @author Yuanhao
@@ -13,10 +19,27 @@ public class InsertController {
 
     @FXML
     private void insert() {
-        if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 1) {
-            Platform.exit();
-        } else if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 0) {
-            System.out.println(123456);
+        if (toolbarPopupList.getSelectionModel().getSelectedIndex() == Function.InsertImage.ordinal()) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("image", "*.png","*.jpeg","*.jpg"));
+            fileChooser.setTitle("打开");
+            File img = fileChooser.showOpenDialog(null);
+
+            if(img != null) {
+                ImageView imageView = new ImageView("file:" + img.getPath());
+                imageView.setFitHeight(300);
+                imageView.setFitWidth(250);
+                Loader.content.getChildren().add(imageView);
+            }
+        } else if (toolbarPopupList.getSelectionModel().getSelectedIndex() == Function.InsertText.ordinal()) {
+
+        } else if(toolbarPopupList.getSelectionModel().getSelectedIndex() == Function.InsertShape.ordinal()) {
+
         }
+    }
+
+    enum Function {
+        // for easy reading
+        InsertImage, InsertText,InsertShape
     }
 }
