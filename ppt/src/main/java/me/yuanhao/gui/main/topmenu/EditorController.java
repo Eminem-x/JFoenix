99c20ce6e9fdc431da94ac5a3099db402f4b69ed
Eminem-x <1773997586@qq.com>
2021-11-15 16:00:26 +0800

    Update EditorController.java

--- ppt/src/main/java/me/yuanhao/gui/main/topmenu/EditorController.java
+++ ppt/src/main/java/me/yuanhao/gui/main/topmenu/EditorController.java
@@ -3,6 +3,7 @@ package me.yuanhao.gui.main.topmenu;
 import com.jfoenix.controls.JFXListView;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import me.yuanhao.AppRun;
 
 /**
  * @author Yuanhao
@@ -11,6 +12,10 @@ public class EditorController {
     @FXML
     private JFXListView<?> toolbarPopupList;
 
+    public EditorController() {
+        judge();
+    }
+
     @FXML
     private void edit() {
         if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 1) {
@@ -18,5 +23,18 @@ public class EditorController {
         } else if (toolbarPopupList.getSelectionModel().getSelectedIndex() == 0) {
             System.out.println(123456);
         }
+    }
+
+    @FXML
+    private void judge() {
+        // disable at start
+        if(toolbarPopupList != null) {
+            toolbarPopupList.setDisable(!AppRun.stage.isFullScreen());
+        }
+
+        // able at showing stage
+        AppRun.stage.heightProperty().addListener(observable -> {
+            toolbarPopupList.setDisable(!AppRun.stage.isFullScreen());
+        });
     }
 }
