dd7ee88a6acdab588ea5499dcba15cd930c2e41d
Eminem-x <1773997586@qq.com>
2021-11-16 17:48:44 +0800

    Create Size.java

---
+++ ppt/src/main/java/me/yuanhao/draw/common/Size.java
@@ -0,0 +1,12 @@
+package me.yuanhao.draw.common;
+
+/**
+ * 定义所有尺寸
+ *
+ * @author Yuanhao
+ */
+public class Size {
+    //画布大小
+    public static final double CANVAS_WIDTH = 1920;
+    public static final double CANVAS_HEIGHT = 1080;
+}
