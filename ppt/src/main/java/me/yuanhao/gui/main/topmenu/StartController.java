22fc543daa67d77d7e5acb8d9d9191ad8dc273fa
Eminem-x <1773997586@qq.com>
2021-11-19 18:57:58 +0800

    Update StartController.java

--- ppt/src/main/java/me/yuanhao/gui/main/topmenu/StartController.java
+++ ppt/src/main/java/me/yuanhao/gui/main/topmenu/StartController.java
@@ -1,7 +1,26 @@
 package me.yuanhao.gui.main.topmenu;
 
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
+import me.yuanhao.mapping.Loader;
+
 /**
  * @author Yuanhao
  */
 public class StartController {
+    @FXML
+    private JFXListView<?> toolbarPopupList;
+
+    @FXML
+    private void start() {
+        int choice = toolbarPopupList.getSelectionModel().getSelectedIndex();
+
+        if(choice == 0) {
+            try {
+                Loader.ppt.getSlides().append();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
